5a40a8869d1cb2735d146ae2d5c4a570d2e21268
W. Santos <w.santos.gg@gmail.com>
2023-06-18 23:54:32 +0800

    can open files

--- src/folderview.ts
+++ src/folderview.ts
@@ -1,11 +1,13 @@
 import {ItemView, WorkspaceLeaf, Plugin, TFolder, TFile } from "obsidian"
+import { MarkdownView } from 'obsidian';
 
 export const FVIEW_TYPE = 'folder-view'
 
 export class FolderView extends ItemView {
     plugin: Plugin
 
-    folderContainer: any
+    folderContainer: HTMLElement
+    noteContainer: HTMLElement
     selectedFolder: TFolder | null
 
     constructor(leaf: WorkspaceLeaf, plugin: Plugin) {
@@ -13,6 +15,9 @@ export class FolderView extends ItemView {
         super(leaf);
 
         this.plugin = plugin
+        this.selectedFolder = this.app.vault.getRoot()
+        this.folderContainer = createDiv()
+        this.noteContainer = createDiv()
     }
 
     getViewType() {
@@ -20,14 +25,80 @@ export class FolderView extends ItemView {
     }
 
     getDisplayText() {
-        return "Example view";
+        return "Folder View";
     }
 
-    getFolders() {
+    makeFolderElement(folder: TFolder): HTMLElement {
+        let folderElement = createDiv()
 
+        folderElement.createEl('b', {text: folder.name})
+        folderElement.onClickEvent((e) => {
+            this.selectedFolder = folder
+            this.getFolders()
+            this.getNotes()
+        })
+
+        return folderElement
+    }
+
+    makeNoteElement(file: TFile): HTMLElement {
+        let noteElement = createDiv()
+
+        noteElement.createEl('span', {text: file.name})
+        noteElement.onClickEvent(async (e) => {
+            this.app.workspace.getLeaf().openFile(file)
+        })
+        
+
+        return noteElement
+    }
+
+    getSelectedFolder(): TFolder {
+        let targetFolder = this.selectedFolder
+
+        if(targetFolder == null) {
+            targetFolder = this.app.vault.getRoot()
+        }
+        return targetFolder
+    }
+
+    async getNotes() {
+        let targetFolder = this.getSelectedFolder()
+        this.noteContainer.empty()
+
+        for(let file of targetFolder.children) {
+            if(file instanceof TFolder){
+                continue
+            }
+
+            this.noteContainer.appendChild(this.makeNoteElement(<TFile>file))
+
+        }
+    }
+
+    async getFolders() {
+        let targetFolder = this.getSelectedFolder()
+        this.folderContainer.empty()
+
+        for(let folder of targetFolder.children) {
+            if(folder instanceof TFile){
+                continue
+            }
+            this.folderContainer.appendChild(this.makeFolderElement(<TFolder>folder))
+        }
     }
 
     async onOpen() {
+        const mainContainer = this.contentEl
+
+        mainContainer.createEl('h1', {text: 'Folders'})
+        mainContainer.append(this.folderContainer)
+        await this.getFolders()
+
+        mainContainer.createEl('h1', {text: 'Notes'})
+        mainContainer.append(this.noteContainer)
+        await this.getNotes()
+
 
     }
 
